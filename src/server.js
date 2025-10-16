507081391503c808e3ade852fea6100585c6055f
Aufa <aufa_romeo@live.com>
2025-10-17 02:43:07 +0800

    FEAT: labs completed

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const logger = require('morgan');
+
+const cors = require('cors');
+
+const petRouter = require('../routes/pets.js');
+
+app.use(cors());
+
+////////////////////////////////////////////////////////////////
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+////////////////////////////////////////////////////////////////
+
+app.use(express.json());
+app.use(logger('dev'));
+
+app.use('/pets', petRouter);
+
+
+////////////////////////////////////////////////////////////////
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
